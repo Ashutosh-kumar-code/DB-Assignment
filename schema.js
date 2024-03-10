2af0026adc7dbdb63e24bef35b2b5b409e32df59
Ashutosh-kumar-code <86124272+Ashutosh-kumar-code@users.noreply.github.com>
2024-03-10 22:37:27 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,1 +1,29 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  tagline: {
+    type: String,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = {
+  Product,
+};
 
